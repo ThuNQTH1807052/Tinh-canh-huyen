17844efc3bcd9b264bcd80423bae9ab074660d4c
quynhthu <thunqth1807052@fpt.edu.vn>
2018-10-12 21:12:33 +0700

    Them dk canh tam giac lon hon 0

--- main.c
+++ main.c
@@ -4,18 +4,20 @@
 //Viết chương trình cho phép người dùng nhập vào 2 cạnh của một tam giác vuông (không phải cạnh huyền).
 //Viết hàm tính chiều dài cạnh huyền của tam giác và in ra kết qủa. Lưu ý độ dài 2 cạnh không được nhỏ hơn 0.
 
-//Ham tinh canh huyen
 float tinhCanhHuyen(int a, int b) {
     return sqrt(a * a + b * b);
 }
 
 int main() {
-    //nhap do dai hai canh
     int a, b;
     printf("Nhap canh goc vuong thu nhat: ");
     scanf("%d", &a);
     printf("Nhap canh goc vuong thu hai: ");
     scanf("%d", &b);
-    printf("\nDo dai canh huyen la: %.2f", tinhCanhHuyen(a, b));
+    if (a <= 0 || b <= 0){
+        printf("Do dai canh tam giac khong duoc nho hon 0.\n");
+    } else {
+        printf("\nDo dai canh huyen la: %.2f", tinhCanhHuyen(a, b));
+    }
     return 0;
 }
